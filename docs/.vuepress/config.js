214aadcad3cd80b40f4caf7c0396d22e0c7a3bfe
Bichi Kim <bichi@live.co.kr>
2018-12-21 00:49:23 +0900

    update 폴더 구조 변경

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,25 @@
 const _ = require('lodash')
+const fs = require('fs')
+const {join} = require('path')
 
 const joinPath = (a, b) => (
   `${_.trimEnd(a, '/')}/${_.trimStart(b, '/')}`
 )
 
 const px = (path, sidebar) => {
-  return sidebar.map((value) => {
+  const paths = sidebar.map((value) => {
     if(typeof  value === 'string'){
       return joinPath(path, value)
     }
     return [joinPath(path, value[0]), value[1]]
+  })
+
+  return _.remove(paths, (value) => {
+    const path = typeof value === 'string' ? value : value[0]
+    const originPath = join(process.cwd(), path)
+    return fs.existsSync(`${originPath}.md`)
+      || fs.existsSync(`${originPath}.html`)
+      || fs.existsSync(join(originPath, 'README.md'))
   })
 }
 
