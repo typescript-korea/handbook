c4906630283c97a13f32bb4a867dde0981168b20
Bichi Kim <bichi@live.co.kr>
2018-12-20 17:11:26 +0900

    update 다국어 설정 추가

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,15 +1,51 @@
+const _ = require('lodash')
+
+const joinPath = (a, b) => (
+  `${_.trimEnd(a, '/')}/${_.trimStart(b, '/')}`
+)
+
+const px = (path, sidebar) => {
+  return sidebar.map((value) => {
+    if(typeof  value === 'string'){
+      return joinPath(path, value)
+    }
+    return [joinPath(path, value[0]), value[1]]
+  })
+}
+
+const sidebar = [
+  '/introduction'
+]
+
+
 module.exports = {
-  base: 'typescript-korea.github.io',
+  base: process.env.NODE_ENV === 'production' ? 'typescript-korea.github.io' :'/',
   locales: {
     '/': {
       lang: 'ko-KR',
       title: '형태명령 손책',
       description: '타입스크립트 스터티용 핸드북입니다.'
     },
-    'en': {
+    '/en/': {
       lang: 'en-US',
       title: 'Typescript Handbook',
       description: 'Typescript handbook for study'
     }
+  },
+  themeConfig: {
+    locales: {
+      '/': {
+        sidebar: px('/',[
+          ['/', '홈'],
+          ...sidebar
+        ])
+      },
+      '/en/': {
+        sidebar: px('/en/',[
+          ['/', 'home'],
+          ...sidebar
+        ])
+      }
+    },
   }
 }
