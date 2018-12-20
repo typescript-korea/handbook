7bec08013b02b3f25be32b2287c19e476d3e8061
Bichi Kim <bichi@live.co.kr>
2018-12-20 15:38:49 +0900

    update 다국어 설정 추가

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  base: 'typescript-korea.github.io'
+  base: 'typescript-korea.github.io',
+  locales: {
+    '/': {
+      lang: 'ko-KR',
+      title: '형태명령 손책',
+      description: '타입스크립트 스터티용 핸드북입니다.'
+    },
+    'en': {
+      lang: 'en-US',
+      title: 'Typescript Handbook',
+      description: 'Typescript handbook for study'
+    }
+  }
 }
